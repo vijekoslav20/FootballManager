9b626da65b289d57123cfcd2c3f42a69f835d594
vijekoslav20 <137533220+vijekoslav20@users.noreply.github.com>
2023-06-25 11:30:22 +0200

    Delete dbValues directory

--- dbValues/insert_into_klub.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO KLUB(naziv_kluba, drzava, trener_id, stadion_id) 
-VALUES ('Real Madrid', 'Španjolska', '1', '1');
-INSERT INTO KLUB(naziv_kluba, drzava, trener_id, stadion_id) 
-VALUES ('AC Milan', 'Italija', '2', '2');
-INSERT INTO KLUB(naziv_kluba, drzava, trener_id, stadion_id) 
-VALUES ('Arsenal', 'Engleska', '3', '3');
--- dbValues/insert_into_trener.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO TRENER(ime, prezime, datum_rodenja, drzavljanstvo, omiljena_formacija) 
-VALUES ('Carlo', 'Ancelotti', '1959-06-10', 'Italija', '4-3-3');
-INSERT INTO TRENER(ime, prezime, datum_rodenja, drzavljanstvo, omiljena_formacija) 
-VALUES ('Stefano', 'Pioli', '1965-10-20', 'Italija', '4-2-3-1');
-INSERT INTO TRENER(ime, prezime, datum_rodenja, drzavljanstvo, omiljena_formacija) 
-VALUES ('Mikel', 'Arteta', '1982-03-26', 'Španjolska', '4-3-3');
