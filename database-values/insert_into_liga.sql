09acacd1510bfb7f844141661c6185836c17245f
vijekoslav20 <137533220+vijekoslav20@users.noreply.github.com>
2023-06-25 11:30:38 +0200

    Add files via upload

---
+++ database-values/insert_into_liga.sql
@@ -0,0 +1,6 @@
+INSERT INTO LIGA(klub_id, bodovi, odigrano, zabijeno, primljeno) 
+VALUES ('1', '0', '0', '0', '0');
+INSERT INTO LIGA(klub_id, bodovi, odigrano, zabijeno, primljeno) 
+VALUES ('2', '0', '0', '0', '0');
+INSERT INTO LIGA(klub_id, bodovi, odigrano, zabijeno, primljeno) 
+VALUES ('3', '0', '0', '0', '0');
